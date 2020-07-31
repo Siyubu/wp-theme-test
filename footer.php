78c0400bd9686281ca211a07d6911be4e8185896
siyubu <solangeiyubu@gmail.com>
2020-07-31 12:37:49 +0200

    wp-theme-test

--- footer.php
+++ footer.php
@@ -1,7 +1,10 @@
 
 	<footer>
-		<h5><?php echo esc_html( Theme_Utils::instance()->footer_text() ); ?></h5>
-		<p><?php echo esc_html__( 'Kapp &copy; 2020', 'kapp-test-theme' ); ?></p>
+		<h5>
+		<?php echo esc_html( Theme_Utils::instance()->footer_text() ); ?>
+		<?php echo esc_html__(Theme_Utils::instance()->get_theme_version() ); ?>
+		</h5>
+		<p><?php echo esc_html__( 'Kapp &copy; 2020', 'kapp-test-theme'); ?></p>
 	</footer>
 	<?php wp_footer(); ?>
 </body>
